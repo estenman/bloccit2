76206120b1a846ec2bf160510a0f5d0c66b48bae
Ericka Stenman <elstenman@gmail.com>
2017-08-26 13:00:46 -0700

    Assignment 19 crud: complete test fix

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+include RandomData
 
 RSpec.describe AdvertisementsController, type: :controller do
   let(:the_ad) { Advertisement.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5)}
@@ -52,16 +53,16 @@ RSpec.describe AdvertisementsController, type: :controller do
   describe "POST create" do
 
     it "increases the number of Advertisement by 1" do
-      expect{ advertisement :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}}.to change(Advertisement,:count).by(1)
+      expect{ post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}}.to change(Advertisement,:count).by(1)
     end
 
     it "assigns the new advertisements to @advertisement" do
-      advertisements :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}
       expect(assigns(:advertisement)).to eq Advertisement.last
     end
 
     it "redirects to the new advertisement" do
-      advertisements :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 5}
       expect(response).to redirect_to Advertisement.last
     end
   end

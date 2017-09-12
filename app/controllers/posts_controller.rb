5e2fa2402ac904d15a7ab1267d4bf9acd8c14c9d
Ericka Stenman <elstenman@gmail.com>
2017-09-11 21:08:03 -0700

    Assignment 28 Authorization: attempt

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_user, except: [:show, :destroy]
+  before_action :authorize_admin_user, except: [:show, :new, :create, ]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -58,10 +60,18 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
-  def authorize_user
+  def authorize_admin_user
     post = Post.find(params[:id])
     unless current_user == post.user || current_user.admin?
       flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
+  end
+
+  def authorize_user
+    post = Post.find(params[:id])
+    unless current_user == post.user || current_user.moderator?
+      flash[:alert] = "You must be an admin or moderator to do that."
       redirect_to [post.topic, post]
     end
   end
